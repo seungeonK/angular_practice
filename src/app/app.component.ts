4f20ea05cdb7d38b021d19143fcf7ca73587701a
seungeonK <hjk6281@gmail.com>
2022-07-26 00:14:43 -0400

    mess around with app/, string interpolation, typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title: string = 'this string is made from "app.component.ts"';
 }
